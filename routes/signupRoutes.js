d20f52464380759b213d1904bc0fa35fd76f7270
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-11-23 21:26:27 +1100

    log-in and links to disease pages fixed to fix express

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -1,30 +1,30 @@
 const db = require("../models");
 
 module.exports = function (app) {
-
-    app.post("api/login", (req, res) => {
-        res.json({
-            fname: req.Person.fname,
-            email: req.Person.email,
-            id: req.Person.id
-        });
+  app.post("/api/login", (req, res) => {
+    res.json({
+      fname: req.body.name,
+      email: req.body.email,
+      id: req.body.id,
     });
+  });
 
-    app.post("/api/signup", (req, res) => {
-        db.Person.create({
-            fname: req.body.fname,
-            email: req.body.email,
-            pass: req.body.pass
-        }).then(() => {
-            res.redirect(307, "/api/login");
-        }).catch(err => {
-            res.status(401).json(err);
-        });
-    });
+  app.post("/api/signup", (req, res) => {
+    db.Person.create({
+      fname: req.body.fname,
+      email: req.body.email,
+      pass: req.body.pass,
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch((err) => {
+        res.status(401).json(err);
+      });
+  });
 
-    app.get("logout", (req, res) => {
-        req.logout();
-        res.redirect("/");
-    });
-
-}
+  app.get("logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+};
