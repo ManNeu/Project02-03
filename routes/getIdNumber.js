9665e90c842af22b73b6dd902243a6b2acdf783a
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-11-24 21:05:01 +1100

    parsing id number of the user

---
+++ routes/getIdNumber.js
@@ -0,0 +1,12 @@
+// var path = require("path");
+// const db = require("../models");
+
+// module.exports = function (app) {
+//   app.get("/api/list", function (req, res) {
+//     db.Profile.create({
+//       person_id:
+//       disease_id: ,
+//       protected: 0,
+//     });
+//   });
+// };
