1e55968e1a90b72740c70a2fa80ae5f9609d726f
Manish Neupane <mrnish.are@gmail.com>
2020-11-26 21:57:05 +1100

    add .env changes

--- models/index.js
+++ models/index.js
@@ -1,43 +1,48 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
+const fs = require('fs');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const basename = path.basename(module.filename);
+const config = {
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'my_database_name',
+  host: process.env.DB_HOSTNAME || 'localhost',
+  port: process.env.DB_PORT || 3306,
+};
+const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  port: config.port,
+  dialect: 'mysql',
+});
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
   })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+
 // JOINING TABLES FOR PROFILE
-db.Disease.hasMany(db.Profile, {foreignKey: 'disease_id'})
-db.Profile.belongsTo(db.Disease, {foreignKey: 'disease_id'})
+db.Disease.hasMany(db.Profile, { foreignKey: 'disease_id' })
+db.Profile.belongsTo(db.Disease, { foreignKey: 'disease_id' })
 // Post.find({ where: { ...}, include: [Disease]})
 
 // JOINING TABLES FOR DESTINATIONS
-db.Disease.hasMany(db.Recomendation, {foreignKey: 'disease_id'})
-db.Recomendation.belongsTo(db.Disease, {foreignKey: 'disease_id'})
+db.Disease.hasMany(db.Recomendation, { foreignKey: 'disease_id' })
+db.Recomendation.belongsTo(db.Disease, { foreignKey: 'disease_id' })
 
 // db.Disease.belongsToMany(db.Profile, {through: 'Profile', foreignKey: 'disease_id'});
 // db.Profile.belongsToMany(db.Disease, {through: 'Profile', foreignKey: 'person_id'});
@@ -56,4 +61,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-    
