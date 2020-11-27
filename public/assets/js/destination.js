3b6d472348bd9da8c1efbdd38bb1c094ab79601f
Manish Neupane <mrnish.are@gmail.com>
2020-11-27 18:26:10 +1100

    routing for all most and some

--- public/assets/js/destination.js
+++ public/assets/js/destination.js
@@ -1,27 +1,20 @@
 var $recomendationAllContainer = $(".recomendation-all-container");
 var $recomendationMostContainer = $(".recomendation-most-container");
-var $recomendationSomeContainer = $(".recomendation-some-container");
-
-
 var userChoiceCountry;
-
 window.onload = function () {
   var userChoiceCountry = localStorage.getItem("country");
   $(".contInfo h2").append(userChoiceCountry);
 };
-
-
 getRecomendationsAll();
-
-
-
+getRecomendationsMost();
+// getRecomendationsSome();
 function getRecomendationsAll() {
-  $.get("/api/recomendation", function (data) {
+  var userChoiceCountry = localStorage.getItem("country");
+  $.get(`/api/recomendationAll/${userChoiceCountry}`, function (data) {
     console.log(data);
     initializeRowsAll(data);
   });
 }
-
 function initializeRowsAll(recomendationsAllRows) {
   $recomendationAllContainer.empty();
   var rowsAllToAdd = [];
@@ -30,7 +23,6 @@ function initializeRowsAll(recomendationsAllRows) {
   }
   $recomendationAllContainer.prepend(rowsAllToAdd);
 }
-
 //   // This function constructs a protection-item row
 function createAllRow(recomendationAll) {
   console.log(recomendationAll);
@@ -47,7 +39,6 @@ function createAllRow(recomendationAll) {
       "</li>"
     ].join("")
   );
-
   $newInputRow.find("button.delete").data("id", recomendationAll.id);
   $newInputRow.find("input.edit").css("display", "none");
   $newInputRow.data("protection", recomendationAll);
@@ -56,15 +47,15 @@ function createAllRow(recomendationAll) {
   }
   return $newInputRow;
 }
-getRecomendationsMost();
-// This function constructs a recommendations-for most
+
+
 function getRecomendationsMost() {
-  $.get("/api/recomendation", function (data) {
+  var userChoiceCountry = localStorage.getItem("country");
+  $.get(`/api/recomendationMost/${userChoiceCountry}`, function (data) {
     console.log(data);
     initializeRowsMost(data);
   });
 }
-
 function initializeRowsMost(recomendationsMostRows) {
   $recomendationMostContainer.empty();
   var rowsMostToAdd = [];
@@ -73,9 +64,8 @@ function initializeRowsMost(recomendationsMostRows) {
   }
   $recomendationMostContainer.prepend(rowsMostToAdd);
 }
-
 //   // This function constructs a protection-item row
-function createMostRow(recomendationMost) {
+function createMostRow(recomendationAll) {
   console.log(recomendationMost);
   var $newInputRow = $(
     [
@@ -90,54 +80,158 @@ function createMostRow(recomendationMost) {
       "</li>"
     ].join("")
   );
-
   $newInputRow.find("button.delete").data("id", recomendationMost.id);
   $newInputRow.find("input.edit").css("display", "none");
-  $newInputRow.data("protection", recomendationMost);
+  $newInputRow.data("protection", recomendationAll);
   if (recomendationMost.complete) {
     $newInputRow.find("span").css("text-decoration", "line-through");
   }
   return $newInputRow;
 }
-getRecomendationsSome();
-function getRecomendationsSome() {
-  $.get("/api/recomendation", function (data) {
-    console.log(data);
-    initializeRowsSome(data);
-  });
-}
 
-function initializeRowsSome(recomendationsSomeRows) {
-  $recomendationSomeContainer.empty();
-  var rowsSomeToAdd = [];
-  for (var i = 0; i < recomendationsSomeRows.length; i++) {
-    rowsSomeToAdd.push(createSomeRow(recomendationsSomeRows[i]));
-  }
-  $recomendationSomeContainer.prepend(rowsSomeToAdd);
-}
 
-//   // This function constructs a protection-item row
-function createSomeRow(recomendationSome) {
-  console.log(recomendationSome);
-  var $newInputRow = $(
-    [
-      "<li class='list-group-item protection-item'>",
-      "<span>",
-      // protection.disease_id, 
-      recomendationSome.Disease.disease,
-      " ",
-      "<button class='complete btn btn-primary'> vaccinate </button>",
-      "</span>",
-      "<input type='text' class='edit' style='display: none;'>",
-      "</li>"
-    ].join("")
-  );
+// var $recomendationAllContainer = $(".recomendation-all-container");
 
-  $newInputRow.find("button.delete").data("id", recomendationSome.id);
-  $newInputRow.find("input.edit").css("display", "none");
-  $newInputRow.data("protection", recomendationSome);
-  if (recomendationSome.complete) {
-    $newInputRow.find("span").css("text-decoration", "line-through");
-  }
-  return $newInputRow;
-}
+// var $recomendationSomeContainer = $(".recomendation-some-container");
+
+
+// var userChoiceCountry;
+
+// window.onload = function () {
+//   var userChoiceCountry = localStorage.getItem("country");
+//   $(".contInfo h2").append(userChoiceCountry);
+// };
+
+
+// getRecomendationsAll();
+
+
+
+// function getRecomendationsAll() {
+//   var userChoiceCountry = localStorage.getItem("country");
+//   $.get(`/api/recomendation/${userChoiceCountry}`, function (data) {
+
+//     console.log(data);
+//     initializeRowsAll(data);
+//   });
+// }
+
+// function initializeRowsAll(recomendationsAllRows) {
+//   $recomendationAllContainer.empty();
+//   var rowsAllToAdd = [];
+//   for (var i = 0; i < recomendationsAllRows.length; i++) {
+//     rowsAllToAdd.push(createAllRow(recomendationsAllRows[i]));
+//   }
+//   $recomendationAllContainer.prepend(rowsAllToAdd);
+// }
+
+// //   // This function constructs a protection-item row
+// function createAllRow(recomendationAll) {
+//   console.log(recomendationAll);
+//   var $newInputRow = $(
+//     [
+//       "<li class='list-group-item protection-item'>",
+//       "<span>",
+//       // protection.disease_id, 
+//       recomendationAll.Disease.disease,
+//       " ",
+//       "<button class='complete btn btn-primary'> vaccinate </button>",
+//       "</span>",
+//       "<input type='text' class='edit' style='display: none;'>",
+//       "</li>"
+//     ].join("")
+//   );
+
+//   $newInputRow.find("button.delete").data("id", recomendationAll.id);
+//   $newInputRow.find("input.edit").css("display", "none");
+//   $newInputRow.data("protection", recomendationAll);
+//   if (recomendationAll.complete) {
+//     $newInputRow.find("span").css("text-decoration", "line-through");
+//   }
+//   return $newInputRow;
+// }
+// getRecomendationsMost();
+// // This function constructs a recommendations-for most
+// function getRecomendationsMost() {
+//   $.get("/api/recomendation", function (data) {
+//     console.log(data);
+//     initializeRowsMost(data);
+//   });
+// }
+
+// function initializeRowsMost(recomendationsMostRows) {
+//   $recomendationMostContainer.empty();
+//   var rowsMostToAdd = [];
+//   for (var i = 0; i < recomendationsMostRows.length; i++) {
+//     rowsMostToAdd.push(createMostRow(recomendationsMostRows[i]));
+//   }
+//   $recomendationMostContainer.prepend(rowsMostToAdd);
+// }
+
+// //   // This function constructs a protection-item row
+// function createMostRow(recomendationMost) {
+//   console.log(recomendationMost);
+//   var $newInputRow = $(
+//     [
+//       "<li class='list-group-item protection-item'>",
+//       "<span>",
+//       // protection.disease_id, 
+//       recomendationMost.Disease.disease,
+//       " ",
+//       "<button class='complete btn btn-primary'> vaccinate </button>",
+//       "</span>",
+//       "<input type='text' class='edit' style='display: none;'>",
+//       "</li>"
+//     ].join("")
+//   );
+
+//   $newInputRow.find("button.delete").data("id", recomendationMost.id);
+//   $newInputRow.find("input.edit").css("display", "none");
+//   $newInputRow.data("protection", recomendationMost);
+//   if (recomendationMost.complete) {
+//     $newInputRow.find("span").css("text-decoration", "line-through");
+//   }
+//   return $newInputRow;
+// }
+// getRecomendationsSome();
+// function getRecomendationsSome() {
+//   $.get("/api/recomendationSome", function (data) {
+//     console.log(data);
+//     initializeRowsSome(data);
+//   });
+// }
+
+// function initializeRowsSome(recomendationsSomeRows) {
+//   $recomendationSomeContainer.empty();
+//   var rowsSomeToAdd = [];
+//   for (var i = 0; i < recomendationsSomeRows.length; i++) {
+//     rowsSomeToAdd.push(createSomeRow(recomendationsSomeRows[i]));
+//   }
+//   $recomendationSomeContainer.prepend(rowsSomeToAdd);
+// }
+
+// //   // This function constructs a protection-item row
+// function createSomeRow(recomendationSome) {
+//   console.log(recomendationSome);
+//   var $newInputRow = $(
+//     [
+//       "<li class='list-group-item protection-item'>",
+//       "<span>",
+//       // protection.disease_id, 
+//       recomendationSome.Disease.disease,
+//       " ",
+//       "<button class='complete btn btn-primary'> vaccinate </button>",
+//       "</span>",
+//       "<input type='text' class='edit' style='display: none;'>",
+//       "</li>"
+//     ].join("")
+//   );
+
+//   $newInputRow.find("button.delete").data("id", recomendationSome.id);
+//   $newInputRow.find("input.edit").css("display", "none");
+//   $newInputRow.data("protection", recomendationSome);
+//   if (recomendationSome.complete) {
+//     $newInputRow.find("span").css("text-decoration", "line-through");
+//   }
+//   return $newInputRow;
+// }
--- routes/destination-api-routes.js
+++ routes/destination-api-routes.js
@@ -1,19 +1,5 @@
-// *********************************************************************************
-// profile-api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our Protection model
-var db = require("../models");
-
-// Routes
-// =============================================================
 module.exports = function (app) {
-
   // var userChoiceCountry;
-
   // window.onload = function () {
   // var userChoiceCountry = localStorage.getItem("country");
   // $(".contInfo h2").append(userChoiceCountry);
@@ -34,14 +20,15 @@ module.exports = function (app) {
   //   });
   // });
 
+  var db = require("../models");
   // GET route for getting all of the protections
-  app.get("/api/recomendation", function (req, res) {
+  app.get("/api/recomendationAll/:country", function (req, res) {
     // findAll returns all entries for a table when used with no options
     db.Recomendation.findAll({
       attributes: ['country_id', 'disease_id', 'priority'],
       where: {
         priority: "all",
-        country_id: "angola"
+        country_id: req.params.country
       },
       include: [db.Disease]
     }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
@@ -50,7 +37,6 @@ module.exports = function (app) {
       res.json(dbRecomendation);
     });
   });
-
   // GET route for getting all of the recoms
   app.get("/api/disease", function (req, res) {
     // findAll returns all entries for a table when used with no options
@@ -62,13 +48,14 @@ module.exports = function (app) {
       res.json(dbDisease);
     });
   });
-  app.get("/api/recomendation", function (req, res) {
+
+  app.get("/api/recomendationMost/:country", function (req, res) {
     // findAll returns all entries for a table when used with no options
     db.Recomendation.findAll({
       attributes: ['country_id', 'disease_id', 'priority'],
       where: {
         priority: "most",
-        country_id: "angola"
+        country_id: req.params.country
       },
       include: [db.Disease]
     }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
@@ -77,7 +64,6 @@ module.exports = function (app) {
       res.json(dbRecomendation);
     });
   });
-
   // GET route for getting all of the recoms
   app.get("/api/disease", function (req, res) {
     // findAll returns all entries for a table when used with no options
@@ -89,33 +75,169 @@ module.exports = function (app) {
       res.json(dbDisease);
     });
   });
-  app.get("/api/recomendation", function (req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Recomendation.findAll({
-      attributes: ['country_id', 'disease_id', 'priority'],
-      where: {
-        priority: "some",
-        country_id: "angola"
-      },
-      include: [db.Disease]
-    }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
-      // We have access to the Profiles as an argument inside of the callback function
-      console.log(dbRecomendation);
-      res.json(dbRecomendation);
-    });
-  });
+}
+  // // DELETE FROM SHOPPING LIST
+  // app.delete("/api/profile/:id", function(req, res) {
+  //   db.Profile.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function(dbProfile) {
+  //     res.json(dbProfile);
+  //   });
+  // });
+  // app.delete("/api/disease", function (req, res) {
+  //   // findAll returns all entries for a table when used with no options
+  //   db.Disease.findAll({
+  //     attributes: ['disease']
+  //   }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+  //     // We have access to the Profiles as an argument inside of the callback function
+  //     console.log(dbDisease);
+  //     res.json(dbDisease);
+  //   });
+  // });
+  // router.delete("/api/profile", function(req, res) {
+  //   var listItem = "id = " + req.params.id;
+  //   app.delete(listItem, function(result) {
+  //     if (result.affectedRows == 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     } else {
+  //       res.status(200).end();
+  //     }
+  //   });
+  // });
+  // };
 
-  // GET route for getting all of the recoms
-  app.get("/api/disease", function (req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Disease.findAll({
-      attributes: ['disease']
-    }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
-      // We have access to the Profiles as an argument inside of the callback function
-      console.log(dbDisease);
-      res.json(dbDisease);
-    });
-  });
+
+
+
+  // // *********************************************************************************
+  // // profile-api-routes.js - this file offers a set of routes for displaying and saving data to the db
+  // // *********************************************************************************
+
+  // // Dependencies
+  // // =============================================================
+
+  // // Requiring our Protection model
+  // var db = require("../models");
+
+  // // Routes
+  // // =============================================================
+  // module.exports = function (app) {
+
+  //   // var userChoiceCountry;
+
+  //   // window.onload = function () {
+  //   // var userChoiceCountry = localStorage.getItem("country");
+  //   // $(".contInfo h2").append(userChoiceCountry);
+  //   // };
+  //   // // GET route for getting all of the protections
+  //   // app.get("/api/destinations", function (req, res) {
+  //   //   // findAll returns all entries for a table when used with no options
+  //   //   db.Profile.findAll({
+  //   //     attributes: ['country_id', 'disease_id', 'priority'],
+  //   //     where: {
+  //   //       protected: 1
+  //   //   },
+  //   //     include: [db.Disease]
+  //   //   }).then(function (dbProfile) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+  //   //     // We have access to the Profiles as an argument inside of the callback function
+  //   //     console.log(dbProfile);
+  //   //     res.json(dbProfile);
+  //   //   });
+  //   // });
+
+  //   // GET route for getting all of the protections
+  //   app.get("/api/recomendation:country", function (req, res) {
+  //     // findAll returns all entries for a table when used with no options
+  //     db.Recomendation.findAll({
+  //       attributes: ['country_id', 'disease_id', 'priority'],
+  //       where: {
+  //         priority: "all",
+  //         country_id: req.params.country
+  //       },
+  //       include: [db.Disease]
+  //     }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+  //       // We have access to the Profiles as an argument inside of the callback function
+  //       console.log(dbRecomendation);
+  //       res.json(dbRecomendation);
+  //     });
+  //   });
+
+  //   // GET route for getting all of the recoms
+  //   app.get("/api/disease", function (req, res) {
+  //     // findAll returns all entries for a table when used with no options
+  //     db.Disease.findAll({
+  //       attributes: ['disease']
+  //     }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+  //       // We have access to the Profiles as an argument inside of the callback function
+  //       console.log(dbDisease);
+  //       res.json(dbDisease);
+  //     });
+  //   });
+
+
+
+
+//   app.get("/api/recomendation", function (req, res) {
+//     // findAll returns all entries for a table when used with no options
+//     db.Recomendation.findAll({
+//       attributes: ['country_id', 'disease_id', 'priority'],
+//       where: {
+//         priority: "most",
+//         country_id: req.params.country
+//       },
+//       include: [db.Disease]
+//     }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+//       // We have access to the Profiles as an argument inside of the callback function
+//       console.log(dbRecomendation);
+//       res.json(dbRecomendation);
+//     });
+//   });
+
+//   // GET route for getting all of the recoms
+//   app.get("/api/disease", function (req, res) {
+//     // findAll returns all entries for a table when used with no options
+//     db.Disease.findAll({
+//       attributes: ['disease']
+//     }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+//       // We have access to the Profiles as an argument inside of the callback function
+//       console.log(dbDisease);
+//       res.json(dbDisease);
+//     });
+//   });
+// };
+  // app.get("/api/recomendationSome", function (req, res) {
+  //   // findAll returns all entries for a table when used with no options
+  //   db.Recomendation.findAll({
+  //     attributes: ['country_id', 'disease_id', 'priority'],
+  //     where: {
+  //       priority: "some",
+  //       country_id: req.params.country
+  //     },
+  //     include: [db.Disease]
+  //   }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+  //     // We have access to the Profiles as an argument inside of the callback function
+  //     console.log(dbRecomendation);
+  //     res.json(dbRecomendation);
+  //   });
+  // });
+
+  // // GET route for getting all of the recoms
+  // app.get("/api/disease", function (req, res) {
+  //   // findAll returns all entries for a table when used with no options
+  //   db.Disease.findAll({
+  //     attributes: ['disease']
+  //   }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+  //     // We have access to the Profiles as an argument inside of the callback function
+  //     console.log(dbDisease);
+  //     res.json(dbDisease);
+  //   });
+  // });
+
+
+
 
 
   // // DELETE FROM SHOPPING LIST
@@ -155,4 +277,4 @@ module.exports = function (app) {
   // });
 
 
-};
+// };
