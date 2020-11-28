1004c9e07cfb3b809b5e4115bd6d08c3625ae609
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2020-11-28 23:45:49 +1100

    tidy up dest

--- routes/destination-api-routes.js
+++ routes/destination-api-routes.js
@@ -19,6 +19,22 @@ module.exports = function (app) {
     });
   });
 
+  app.get("/api/recomendationMost/:country", function (req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Recomendation.findAll({
+      attributes: ['country_id', 'disease_id', 'priority'],
+      where: {
+        priority: "most",
+        country_id: req.params.country
+      },
+      include: [db.Disease]
+    }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+      // We have access to the Profiles as an argument inside of the callback function
+      console.log(dbRecomendation);
+      res.json(dbRecomendation);
+    });
+  });
+
   app.get("/api/recomendationSome/:country", function (req, res) {
     // findAll returns all entries for a table when used with no options
     db.Recomendation.findAll({
@@ -35,31 +51,15 @@ module.exports = function (app) {
     });
   });
 
-  // GET route for getting all of the recoms
-  app.get("/api/disease", function (req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Disease.findAll({
-      attributes: ['disease', 'id']
-    }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
-      // We have access to the Profiles as an argument inside of the callback function
-      console.log(dbDisease);
-      res.json(dbDisease);
+    // GET route for getting all of the recoms
+    app.get("/api/disease", function (req, res) {
+      // findAll returns all entries for a table when used with no options
+      db.Disease.findAll({
+        attributes: ['disease', 'id']
+      }).then(function (dbDisease) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
+        // We have access to the Profiles as an argument inside of the callback function
+        console.log(dbDisease);
+        res.json(dbDisease);
+      });
     });
-  });
-
-  app.get("/api/recomendationMost/:country", function (req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Recomendation.findAll({
-      attributes: ['country_id', 'disease_id', 'priority'],
-      where: {
-        priority: "most",
-        country_id: req.params.country
-      },
-      include: [db.Disease]
-    }).then(function (dbRecomendation) {    // db.Profile.findAll({}) use SQL fiormula here to filter out data true/false
-      // We have access to the Profiles as an argument inside of the callback function
-      console.log(dbRecomendation);
-      res.json(dbRecomendation);
-    });
-  });
 }
